139323b735ecb0c7f456a6a117618a285af3a9d2
gaganbedi771 <gaganbedi771@gmail.com>
2022-11-11 15:09:18 +0530

    upto 3rd assignment

---
+++ DOM manipulation/dom.js
@@ -0,0 +1,16 @@
+//Examine the document obj
+
+// console.dir(document.URL)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title='Dom practice'
+//console.log(document.getElementById("header1").innerText)
+//document.getElementById("header1").innerText='Abcd';
+//
+//console.log(document.getElementById("header1").textContent)
+//document.getElementById("header1").textContent='Abcd';
+console.log(document.getElementById("header1").textContent)
+document.getElementById('header1').style.borderBottom='solid 3px black';
+
+document.getElementById('header2').style.color='green'
+document.getElementById('header2').style.fontweight="bold";
