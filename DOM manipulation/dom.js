b776377ce52526aa39fac25041f52d2ae56f73a3
gaganbedi771 <gaganbedi771@gmail.com>
2022-11-11 23:32:01 +0530

    upto assignment 6

--- DOM manipulation/dom.js
+++ DOM manipulation/dom.js
@@ -22,9 +22,28 @@
 //     i.style.fontWeight='bold';
 // }
 
-let li=document.getElementsByTagName('li');
-console.log(li);
+// let li=document.getElementsByTagName('li');
+// console.log(li);
 
-for(let i of li){
-    i.style.fontWeight='bold';
+// for(let i of li){
+//     i.style.fontWeight='bold';
+// }
+
+// let item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+
+let titles=document.querySelectorAll('.title');
+//console.log(titles.textContent);
+//titles[1].textContent='Changed'
+let thirdchild=document.querySelector('.list-group-item:nth-child(3)');
+thirdchild.textContent='';
+
+let secondchild=document.querySelector('.list-group-item:nth-child(2)');
+secondchild.style.color='green';
+
+let odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+for(let i of odd){
+    i.style.backgroundColor='green';
 }
