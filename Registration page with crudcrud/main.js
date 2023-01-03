0d55893f71e8d66d676d5944c8bd4ef28508cca8
gaganbedi771 <gaganbedi771@gmail.com>
2023-01-04 00:39:13 +0530

    delete from crud

--- Registration page with crudcrud/main.js
+++ Registration page with crudcrud/main.js
@@ -7,28 +7,13 @@ function onSubmit(e) {
     e.preventDefault();
     let nameInput = document.getElementById('name');
     let emailInput = document.getElementById('email');
-    // localStorage.setItem('name',nameInput.value);
-    // localStorage.setItem('email',emailInput.value);
-
-    //if email(which is unique) already present in db, remove the previous value from screen
-
-    if (JSON.parse(localStorage.getItem(emailInput.value)) !== null) {
-        let childToRemove = document.getElementById(emailInput.value);
-        childToRemove.remove();
-    }
-
-    //save as objects
-    //   localStorage.setItem(emailInput.value,JSON.stringify({
-    //     name:nameInput.value,
-    //     email:emailInput.value
-    //   }));
-    let id;
+    
     axios.post("https://crudcrud.com/api/e07f11eb4df74c589fae41633695886e/appointmentData", { name: nameInput.value, email: emailInput.value })
         .then((res) => {
             console.log(res);
             axios.get("https://crudcrud.com/api/e07f11eb4df74c589fae41633695886e/appointmentData")
                 .then((res1) => {
-                    id = res1.data[res1.data.length - 1]._id;
+                    let id = res1.data[res1.data.length - 1]._id;
                     console.log(id)
                     //get all registered users and add to li elements    
                     let li = document.createElement('li');
@@ -66,19 +51,6 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         .catch(err => console.log(err))
 })
-// let keys = Object.keys(localStorage);
-// for (let k of keys) {
-
-//     let n = JSON.parse(localStorage.getItem(k)).name;
-//     let e = JSON.parse(localStorage.getItem(k)).email;
-//     let li = document.createElement('li');
-//     li.className = 'item';
-//     li.id = e;
-//     li.appendChild(document.createTextNode(`${n} ${e} `));
-//     appendButtonToLi(li);
-//     let users = document.getElementById('users');
-//     users.appendChild(li);
-// }
 
 function appendButtonToLi(li) {
     let btnDel = document.createElement('button');
@@ -102,18 +74,6 @@ function alterItem(e) {
         console.log('delete', editId);
         let link = 'https://crudcrud.com/api/e07f11eb4df74c589fae41633695886e/appointmentData/' + editId;
         axios.delete(link);
-        // axios.get("https://crudcrud.com/api/e07f11eb4df74c589fae41633695886e/appointmentData")
-        //     .then((res) => {
-        //         for (let i = 0; i < res.data.length; i++) {
-        //             if (res.data[i].email == editId) {
-        //                 let idToDel = res.data[i]._id;
-        //                 let link = 'https://crudcrud.com/api/e07f11eb4df74c589fae41633695886e/appointmentData/' + idToDel;
-        //                 axios.delete(link);
-        //                 break;
-        //             }
-        //         }
-        //     })
-        //     .catch(err => console.log(err))
         e.target.parentNode.remove();
 
     }
