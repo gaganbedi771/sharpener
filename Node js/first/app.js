8ffe77ab8b1ca223d87611cbade36e49a22a1ed2
gaganbedi771 <gaganbedi771@gmail.com>
2023-01-29 00:36:16 +0530

    welcome to home about node

--- Node js/first/app.js
+++ Node js/first/app.js
@@ -1,11 +1,22 @@
-// const server = http.createServer(function (req,res){
-//     console.log(req);
-//     res.setHeader('Content-Type','text/html')
-// });
-// server.listen(3000);
-
-const http= require('http');
+const http= require("http");
 const server=http.createServer(function(req,res){
-    console.log("Gagan");
+    //console.log("Gagan");
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    
+    if(req.url=='/home'){
+        res.write('<body><h1>Welcome Home</h1></body>')
+    }
+    else if(req.url=='/about'){
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+    }
+    else if(req.url=='/node'){
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    }
+
+    res.write('</html>')
+    res.end();
 })
 server.listen(4000);
