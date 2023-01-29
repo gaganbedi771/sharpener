e6ad7591ae29b9a6d7eee9c50c04d4f2ce6ecae9
gaganbedi771 <gaganbedi771@gmail.com>
2023-01-29 16:04:19 +0530

    write data to file and display from file

--- Node js/first/app.js
+++ Node js/first/app.js
@@ -1,22 +1,54 @@
-const http= require("http");
-const server=http.createServer(function(req,res){
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
     //console.log("Gagan");
+    const method = req.method;
 
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></head>')
-    
-    if(req.url=='/home'){
-        res.write('<body><h1>Welcome Home</h1></body>')
+    if (req.url == '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body>');
+        fs.readFile('message.txt', 'utf8', (err, data) => {
+            res.write(`<p>${data}</p>`);
+            res.write('<h1>Enter Message </h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
+            res.write('</body>');
+            res.write('</html>')
+            return res.end();
+        });
+
     }
-    else if(req.url=='/about'){
+    else if (req.url == '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
         res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+        return res.end();
     }
-    else if(req.url=='/node'){
+    else if (req.url == '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
         res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+        return res.end();
     }
-
-    res.write('</html>')
-    res.end();
+    else if (req.url = '/message' && method == "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedbody = Buffer.concat(body).toString();
+            const message = parsedbody.split('=')[1];
+            fs.writeFile("message.txt", message, (err) => {
+                res.statusCode = 302; //302==redirection
+                res.setHeader('Location', '/home')
+                return res.end();
+            });
+        })
+    }
 })
 server.listen(4000);
