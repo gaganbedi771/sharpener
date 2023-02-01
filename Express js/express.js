3f48e6d3efdfe5fc9f3aadb03aa34cdf0ddb8f74
gaganbedi771 <gaganbedi771@gmail.com>
2023-02-01 21:14:37 +0530

    routers with clean up code and route filtering

--- Express js/express.js
+++ Express js/express.js
@@ -1,25 +1,18 @@
 const express=require('express');
 const app=express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 const bodyParser= require('body-parser');
 app.use (bodyParser.urlencoded({extended: true}));
 
-app.use("/add-product",(req,res,next)=>{
-    //console.log("In middleware");
-    //res.send('<h1>Hello from exsdfsdfsdpress</h1>');
-    res.send('<form action=/product method="POST"><input type="text" name="product" placeholder="Product Name"><input type="number" name="quantity" placeholder="quantity"><button type="submit">Add Product</button></form>')
-    //next();
-})
-
-app.post("/product",(req,res,next)=>{
-    console.log(req.body);
-    res.redirect("/");
-})
-
-app.use("/",(req,res,next)=>{
-    //console.log("In middleware again");
-    res.send('<h1>Hello from express</h1>');
-    //res.send( { key1: 'value' });
+app.use("/admin",adminRoutes);
+app.use(shopRoutes);
+    
+app.use((req,res,next)=>{
+    res.status(404);
+    res.send("<h1>Page Note Found</h1>");
 })
 
 app.listen(3000);
