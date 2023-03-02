0622410ccfcd40d5c35d87a10264b81e27f9630f
gaganbedi771 <gaganbedi771@gmail.com>
2023-03-03 00:37:48 +0530

    sequelize

--- Product Appstarting-setup/util/database.js
+++ Product Appstarting-setup/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    database:"node-complete",
-    password:"toor"
-})
+const sequelize = new Sequelize('node-complete', 'root', 'toor', { dialect: 'mysql', host: 'localhost' });
 
-module.exports=pool.promise();
+module.exports = sequelize;
+
+
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//     host:"localhost",
+//     user:"root",
+//     database:"node-complete",
+//     password:"toor"
+// })
+
+// module.exports=pool.promise();
+
