8ff830d3063464fab77811f0142553389ad5b34c
gaganbedi771 <gaganbedi771@gmail.com>
2023-04-27 23:15:58 +0530

    get started with typescript

---
+++ typeScript/app.ts
@@ -0,0 +1,51 @@
+const num1 = document.getElementById("num1") as HTMLInputElement;
+const num2 = document.getElementById("num2") as HTMLInputElement;
+const buttonElement = document.querySelector("button")!;
+const arrNum = [] as number[];
+const arrStr = [] as string[];
+
+buttonElement.addEventListener("click", () => {
+    const num1val = num1.value;
+    const num2val = num2.value;
+    const result = add(num1val, num2val);
+    // console.log(result);
+
+    console.log(arrNum);
+    console.log(arrStr);
+
+    printResult({ val: result as number })
+})
+
+type numORstr = number | string;
+
+function add(num1: numORstr, num2: numORstr) {
+    if (typeof num1 == "number" && typeof (num2) === "number") {
+        arrNum.push(num1 + num2);
+        return num1 + num2;
+    }
+    else if (typeof num1 == "string" && typeof (num2) === "string") {
+        arrStr.push(num1 + ' ' + num2);
+        return num1 + ' ' + num2;
+    }
+    // console.log("string and number");
+    arrNum.push(+num1 + +num2);
+
+    return +num1 + +num2;
+}
+
+function printResult(resObj: { val: number }) {
+    console.log(resObj.val);
+}
+
+// console.log(add("1","2"));
+// console.log(add(3, 3));
+
+const myPromise=new Promise<String>((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("It worked")
+    },1000)
+})
+
+myPromise.then((result)=>{
+    console.log(result.split("w"))
+})
