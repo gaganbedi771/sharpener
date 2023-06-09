d77ee979bcb375ecd29dc2f36ec8f78819b48a34
gaganbedi771 <gaganbedi771@gmail.com>
2023-06-09 23:42:46 +0530

    Order System

--- Product Appstarting-setup/controllers/shop.js
+++ Product Appstarting-setup/controllers/shop.js
@@ -1,9 +1,10 @@
 const Product = require('../models/product');
 const Cart = require("../models/cart");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   // Product.fetchAll()
-  Product.find() 
+  Product.find()
     .then((products) => {
       res.render('shop/product-list', {
         prods: products,
@@ -49,9 +50,9 @@ exports.getCart = (req, res, next) => {
 
   // req.user.getCart()
   req.user.populate("cart.items.productId")
-  // .execPopulate()
+    // .execPopulate()
     .then(user => {
-      const products=user.cart.items;
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -121,10 +122,28 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-  console.log("running")
-  req.user.addOrder()
+  req.user.populate("cart.items.productId")
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+
+      return order.save();
+    })
+    // req.user.addOrder()
     .then(result => {
-      res.redirect('/orders')
+      return req.user.clearCart()
+        .then(() => {
+          res.redirect('/orders')
+        })
+
     })
     .catch(err => {
       console.log(err);
@@ -132,16 +151,17 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
-  .then(orders=>{
-    res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-      orders: orders
-    });
-  })
-  .catch(err=>console.log(err))
-  
+  // req.user.getOrders()
+  Order.find({"user.userId":req.user._id})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err))
+
 };
 
 exports.getCheckout = (req, res, next) => {
--- Product Appstarting-setup/models/user.js
+++ Product Appstarting-setup/models/user.js
@@ -48,6 +48,10 @@ userSchema.methods.removeFromCart= function(productId){
                 return this.save();
 } 
 
+userSchema.methods.clearCart=function(){
+    this.cart={items:[]};
+    return this.save();
+}
 module.exports=mongoose.model("User",userSchema);
 
 // const Sequelize =require("sequelize");
