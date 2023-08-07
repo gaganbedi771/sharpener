15b0f9c0d113d7f8627f0168f0d55f47d3429197
gaganbedi771 <gaganbedi771@gmail.com>
2023-08-07 13:22:42 +0530

    hardcode expense item

---
+++ react-complete/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2> Expense Item </h2>
+      <div>
+        <h3>Food Rs 10</h3>
+        <h3>Petrol Rs 100</h3>
+        <h3>Movies Rs 200</h3>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
