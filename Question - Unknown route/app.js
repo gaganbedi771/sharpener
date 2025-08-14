541b17a606053ea31b47ceae7e89907260c4dd05
gaganbedi771 <117933792+gaganbedi771@users.noreply.github.com>
2025-08-14 19:26:18 +0530

    Add files via upload

---
+++ Question - Unknown route/app.js
@@ -0,0 +1,26 @@
+const app = require("express")();
+
+app.get("/products", (req, res, next) => {
+    res.send(`<h1>Here is the list of all products .</h1>`);
+});
+app.post("/products", (req, res, next) => {
+    res.send(`<h1>A new product has been added.</h1>`);
+});
+
+app.get("/categories", (req, res, next) => {
+    res.send(`<h1>Here is the list of all categories.</h1>`);
+});
+app.post("/categories", (req, res, next) => {
+    res.send(`<h1>A new categories has been created.</h1>`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+
+app.listen(4000 , (req, res) => {
+  console.log(
+    "Server is up and running on port 3000! Ready to handle requests."
+  );
+});
