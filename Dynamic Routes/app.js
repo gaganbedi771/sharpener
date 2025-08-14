b62a97317ff5455b50877c4d829c060abfe15d7b
gaganbedi771 <117933792+gaganbedi771@users.noreply.github.com>
2025-08-14 20:53:57 +0530

    Add files via upload

---
+++ Dynamic Routes/app.js
@@ -0,0 +1,33 @@
+const app = require("express")();
+
+app.get("/products", (req, res, next) => {
+    res.send(`<h1>Here is the list of all products .</h1>`);
+});
+app.post("/products", (req, res, next) => {
+    res.send(`<h1>A new product has been added.</h1>`);
+});
+
+app.get("/categories", (req, res, next) => {
+    res.send(`<h1>Here is the list of all categories.</h1>`);
+});
+app.post("/categories", (req, res, next) => {
+    res.send(`<h1>A new categories has been created.</h1>`);
+});
+
+app.get("/welcome/:username", (req, res, next) => {
+    const {username}=req.params;
+    const {role}=req.query;
+
+    res.send(`<h1>Welcome ${username}, your role is ${role}</h1>`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+
+app.listen(4000 , (req, res) => {
+  console.log(
+    "Server is up and running on port 4000! Ready to handle requests."
+  );
+});
